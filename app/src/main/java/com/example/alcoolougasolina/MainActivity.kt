a4dfe9283f487e8a5bff8a95136dbff928bfb0c2
Windson Viana <windson@virtual.ufc.br>
2024-03-20 18:46:06 -0300

    Atualizado 2024

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,43 +1,49 @@
 package com.example.alcoolougasolina
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("PDM23","No onCreate, $percentual")
+        Log.i("PDM24","No onCreate, $percentual")
 
+        percentual=0.7
         val btCalc: Button = findViewById(R.id.btCalcular)
+        val textMsg:TextView= findViewById(R.id.textMsg)
+        //btCalc.setBackgroundColor(Color.CYAN)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
             percentual=0.75
-            Log.d("PDM23","No btCalcular, $percentual")
+            textMsg.text="Já tenho o percentual"
+            Log.d("PDM24","No btCalcular, $percentual")
         })
     }
 override fun onResume(){
     super.onResume()
-    Log.d("PDM23","No onResume, $percentual")
+    Log.d("PDM24","No onResume, $percentual")
 }
 override fun onStart(){
     super.onStart()
-    Log.d("PDM23","No onResume")
+    Log.v("PDM24","No onStart")
 }
 override fun onPause(){
     super.onPause()
-    Log.d("PDM23","No onResume")
+    Log.e("PDM24","No onPause")
 }
 override fun onStop(){
     super.onStop()
-    Log.d("PDM23","No onResume")
+    Log.w("PDM24","No onStop")
 }
 override fun onDestroy(){
     super.onDestroy()
-    Log.d("PDM23","No onResume")
+    Log.wtf("PDM24","No Destroy")
 }
 }
