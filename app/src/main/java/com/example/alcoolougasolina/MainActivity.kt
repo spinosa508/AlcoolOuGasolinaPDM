3c2e07c31ce50db0c19fb97b7d67c3a99c4bc48b
Windson Viana <windson@virtual.ufc.br>
2024-04-01 00:01:38 -0300

    Intial Commmit

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,32 +1,56 @@
 package com.example.alcoolougasolina
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    lateinit var edAlcool: EditText
+    lateinit var edGasolina: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("PDM24.1","No onCreate, $percentual")
+        if (savedInstanceState != null) {
+            percentual=savedInstanceState.getDouble("percentual")
+        }
+        Log.d("PDM24", "No onCreate, $percentual")
 
         percentual=0.7
         val btCalc: Button = findViewById(R.id.btCalcular)
         val textMsg:TextView= findViewById(R.id.textMsg)
-        //btCalc.setBackgroundColor(Color.CYAN)
-        btCalc.setOnClickListener(View.OnClickListener {
+        val swPercent: Switch= findViewById(R.id.swPercentual)
+        edAlcool = findViewById(R.id.edAlcool)
+        edGasolina = findViewById(R.id.edGasolina)
+        var gasolinaValor = edGasolina.text
+        var alcoolValor = edAlcool.text
+
+        swPercent.setOnClickListener {
             //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
-            Log.d("PDM24","No btCalcular, $percentual")
-        })
+            percentual = 0.75
+            Log.d("PDM24", "No swPercent, $percentual")
+        }
+
+        btCalc.setOnClickListener {
+            //código do evento
+            textMsg.text = "Gasolina: $gasolinaValor e Alcool: $alcoolValor"
+            Log.d("PDM24", "No btCalcular, $percentual")
+        }
     }
-override fun onResume(){
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putDouble("percentual",percentual)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume(){
     super.onResume()
     Log.d("PDM24","No onResume, $percentual")
 }
